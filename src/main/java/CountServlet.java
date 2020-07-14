3be7603c8fa7b206c0e38c735981f4470ee3865f
Laura Davis <laura.l.davis.210@gmail.com>
2020-07-14 12:10:49 -0500

    Refactor bonus to allow any value for reset param

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -8,22 +8,19 @@ import java.io.PrintWriter;
 @WebServlet(name = "CountServlet", urlPatterns = "/count")
 public class CountServlet extends HttpServlet {
 
-    private int count = 0;
+    private static int count = 0;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        count++;
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
 
         String reset = req.getParameter("reset");
 
-        if(reset == null){
-            count = count;
-        } else if (reset.equalsIgnoreCase("true")){
+        if(reset != null){
             count = 0;
         } else {
-            count = count;
+            count++;
         }
 
         out.println("<h1>This page has been visited " + count + " times.</h1>");
